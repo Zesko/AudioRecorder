e8b76d5e44af4c54ab5faa0515aa4e320539add1
xchen <xchen@novomind.com>
2017-02-03 16:15:15 +0100

    create project

---
+++ app/src/main/java/com/example/xchen/audiorecognition/AudioMain.java
@@ -0,0 +1,13 @@
+package com.example.xchen.audiorecognition;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AudioMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_main);
+    }
+}
